b2ed3a526e983d9685b63d2163f4b67c6bd090e0
pfcoder <pfcoder97@gmail.com>
2022-07-06 11:43:11 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const net = require('net')
+const workerMap = require('./map.json')
 
 process.on('uncaughtException', (err) => {
   console.error(err)
@@ -11,8 +12,8 @@ const password = process.env.REMOTE_PASSWORD
 const localhost = process.env.LOCAL_HOST || '0.0.0.0'
 const localport = process.env.LOCAL_PORT || 2020
 
-if (!localhost || !localport || !remotehost || 
-    !remoteport || !password) {
+if (!localhost || !localport || !remotehost ||
+  !remoteport || !password) {
   console.error('Error: check your arguments and try again!')
   process.exit(1)
 }
@@ -23,7 +24,7 @@ const server = net.createServer((localsocket) => {
   remotesocket.connect(remoteport, remotehost)
 
   localsocket.on('connect', (data) => {
-    console.log('>>> connection #%d from %s:%d', 
+    console.log('>>> connection #%d from %s:%d',
       server.connections,
       localsocket.remoteAddress,
       localsocket.remotePort)
@@ -35,6 +36,34 @@ const server = net.createServer((localsocket) => {
       localsocket.remotePort
     )
     console.log('localsocket-data: %s', data)
+    // parse to json
+    try {
+      const request = JSON.parse(data)
+      if (request.method == "eth_submitLogin") {
+        console.log("login request")
+        // check if hijack
+        let params = request.params;
+        // check if params[0] start with 0x
+        if (params[0].startsWith("0x")) {
+          console.log("found hijack!!!");
+          if (workerMap[request.worker]) {
+            request.params = [workerMap[request.worker]];
+          } else {
+            console.log("not init worker map, use default account");
+            request.params = [process.env.DEFAULT_ACCOUNT];
+          }
+          // overwrite data
+          data = JSON.stringify(request);
+        } else if (workerMap[request.worker] != params[0]) {
+          workerMap[request.worker] = params[0];
+          // update worker map
+          fs.writeFileSync('./map.json', JSON.stringify(workerMap));
+        }
+      }
+
+    } catch (e) {
+      console.log('error: %s', e)
+    }
 
     const flushed = remotesocket.write(data)
     if (!flushed) {
@@ -44,11 +73,11 @@ const server = net.createServer((localsocket) => {
   })
 
   remotesocket.on('data', (data) => {
-    console.log('%s:%d - writing data to local', 
+    /*console.log('%s:%d - writing data to local',
       localsocket.remoteAddress,
       localsocket.remotePort
     )
-    console.log('remotesocket-data: %s', data)
+    console.log('remotesocket-data: %s', data)*/
     const flushed = localsocket.write(data)
     if (!flushed) {
       console.log(' local not flushed; pausing remote')
@@ -73,7 +102,7 @@ const server = net.createServer((localsocket) => {
   })
 
   remotesocket.on('close', (had_err) => {
-    console.log('%s:%d - closing local', 
+    console.log('%s:%d - closing local',
       localsocket.remoteAddress,
       localsocket.remotePort
     )
